5a817ae8ee6a9cf92537e1ae77829f5b9886379e
Manish Mayank <62476722+MannyMayank@users.noreply.github.com>
2022-02-14 07:37:35 +0000

    Multi line string

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #Classic Hello World
 print("Hello World")
+#-----------------------------------------------
+
+#Multi line string, use triple quote
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
