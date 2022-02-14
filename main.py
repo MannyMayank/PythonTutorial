043290dee1905a1dbde0f4e5eb7e3185a8b8ce47
Manish Mayank <62476722+MannyMayank@users.noreply.github.com>
2022-02-14 08:21:09 +0000

    Input function

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("Hello World")
 print("""Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')""")
+#-----------------------------------------------
+
+#Input Function, take input from console or user
+name=input("What is your name?")
+print(name)
